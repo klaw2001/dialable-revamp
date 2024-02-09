e5b33cbc173e01c8ef485bce61388f31b0041369
rohitprmsnls <rohit@prmsnls.xyz>
2024-02-09 23:42:12 +0530

    create connectus section

--- src/app/Components/ConnectUs.jsx
+++ src/app/Components/ConnectUs.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
-
+import React from "react";
+import ConnectBg from "../Images/connect.png";
+import Image from "next/image";
 const ConnectUs = () => {
   return (
-    <div className='m-20 border-2   p-20 gap-4'>
-        <h1 className='text-[2.5vw] montserrat text-[black] font-bold leading-[3vw]'>Boost Your Business with Us</h1>
-        <p className='roboto font-medium text-[1vw]'>Join our directory and reach a wider audience. Increase your visibility and grow your business.</p>
-        <button className='btn bg-[#781393] py-2 px-6 text-[1.2vw] font-medium text-white montserrat'>List</button>
+    <div
+      className={`m-20 border-2  relative   gap-4 after:content-[""] after:w-full after:h-full after:absolute after:top-0 after:left-0 after:bg-[#781393] after:opacity-[.63]`}
+    >
+      <div className="relative">
+        <Image
+          src={ConnectBg}
+          alt="connect"
+          objectFit="cover"
+          className="h-full w-full"
+        />
+      </div>
+      <div className="absolute top-[50%] w-[60%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-[2] text-center">
+        <h1 className="text-[3vw] mb-4 montserrat font-bold leading-[3vw]">
+          Discover Opportunities and Connect Easily
+        </h1>
+        <p className="roboto font-normal text-[1.2vw] mb-2">
+          Join our community and stay updated with the latest news and
+          opportunities
+        </p>
+        <div className="flex items-center justify-center gap-2 my-6 montserrat">
+          <input placeholder="Your Email Address" type="text" className="py-4 px-6 text-black placeholder:text-black" />
+          <button className="btn bg-[#781393] py-4 px-6 text-[1vw] border border-white font-medium text-white montserrat">
+            Get started
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ConnectUs
+export default ConnectUs;
