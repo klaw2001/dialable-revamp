590dde8450d3fd0684ba35cb9a46ed936e9db270
rohitprmsnls <rohit@prmsnls.xyz>
2024-03-05 11:15:52 +0530

    checkout page responsive

--- src/Components/CheckoutPage/Checkout/Checkout.jsx
+++ src/Components/CheckoutPage/Checkout/Checkout.jsx
@@ -6,20 +6,20 @@ import Prod from "../../../../public/Images/wheelchairfront.png";
 const Checkout = () => {
   return (
     <div className="py-10 px-4 lg:p-24 lg:mb-8 relative overflow-x-clip">
-      <div className="breadcrumb mt-10 text-black text-[3vw] md:text-[2vw] lg:text-[1.1vw] font-medium">
+      <div className="breadcrumb mt-0 lg:mt-10 text-black text-[3vw] md:text-[2vw] lg:text-[1.1vw] font-medium">
         <Link href={"/"}>Home</Link> {`>`} <Link href={"/cart"}>Cart</Link>{" "}
         {`>`} Checkout
       </div>
       <h1 className="Sans text-[5vw] md:text-[4vw] lg:text-[3vw] font-bold mb-4 md:mb-6 lg:mb-8">
         Checkout
       </h1>
-      <div className="flex gap-6 flex-col lg:flex-row montserrat">
+      <div className="flex gap-3 md:gap-6 flex-col lg:flex-row montserrat">
         <div className="w-full lg:w-1/2">
           <div className="pb-8 border-b-2 border-black">
             <div className="form montserrat">
               <form>
-                <div className="contact flex flex-col gap-5">
-                  <p className="font-bold text-black text-[1.05vw]">
+                <div className="contact flex flex-col gap-3 md:gap-5">
+                  <p className="font-bold text-black text-[3vw] md:text-[2vw] lg:text-[1.05vw]">
                     Contact Information
                   </p>
 
@@ -47,7 +47,7 @@ const Checkout = () => {
                   </div>
                 </div>
                 <div className="address flex flex-col gap-5 mt-12">
-                  <p className="font-bold text-black text-[1.05vw]">
+                  <p className="font-bold text-black text-[3vw] md:text-[2vw] lg:text-[1.05vw]">
                     Shipping Address
                   </p>
                   <div className="inputCont">
@@ -147,9 +147,9 @@ const Checkout = () => {
                       </label>
                     </div>
                   </div>
-                  <div>
+                  <div className=" flex items-center gap-1">
                     <input type="checkbox"  name="" id="save" />
-                    <label htmlFor="save" className="px-2">Save Address for Future Orders</label>
+                    <label htmlFor="save" className="px-2 text-xs">Save Address for Future Orders</label>
                   </div>
                 </div>
               </form>
@@ -217,7 +217,7 @@ const Checkout = () => {
                 <p className="text-[#000000BF]">Subtotal</p>
                 <p>₹2500 /-</p>
               </div>
-              <button className="px-4 py-4 mt-6 mb-16 w-full bg-[#781393] text-white font-medium active:scale-[.99] transition-all duration-100">
+              <button className="px-4 py-4 mt-6 mb-6 lg:mb-16 w-full bg-[#781393] text-white font-medium active:scale-[.99] transition-all duration-100">
                 Proceed to Payment
               </button>
             </div>
