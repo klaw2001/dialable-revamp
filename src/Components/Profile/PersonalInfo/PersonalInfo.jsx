5bf79a30690dddb137ee4974534ce27d20b3a25b
klaw <netke2611@gmail.com>
2024-03-20 16:11:23 +0530

    profile page respomsive

--- src/Components/Profile/PersonalInfo/PersonalInfo.jsx
+++ src/Components/Profile/PersonalInfo/PersonalInfo.jsx
@@ -3,142 +3,142 @@ import { FaRegUser } from "react-icons/fa6";
 
 const PersonalInfo = () => {
   return (
-    <div className="grow-[.8] pl-8 border-l border-[#00000075]">
+    <div className="grow-[.8] md:pl-8 md:border-l border-[#00000075]">
       <h1 className="montserrat  border-[#00000075]  text-black text-[6vw]  md:text-[4.5vw] font-semibold md:leading-[4.8vw] lg:leading-[3.2vw] lg:text-[2vw]">
         Personal Information
       </h1>
-      <p className="md:text-[1.5vw] lg:text-[0.8vw] montserrat font-semibold">
+      <p className="text-[4vw] md:text-[1.5vw] lg:text-[0.8vw] montserrat font-semibold">
         Manage/Edit your personal information, where you can be contacted.
       </p>
 
-      <div className="my-16">
+      <div className="my-8 md:my-16">
         <form>
-          <div className="flex gap-4 my-8 flex-wrap">
+          <div className="flex gap-4 my-4 md:my-8 flex-wrap">
             <div className="bg-white py-2 grow rounded-md shadow-md px-4">
-              <div className="flex montserrat md:text-[2vw] lg:text-[1vw] font-semibold items-center justify-between">
+              <div className="flex montserrat text-[4.5vw] md:text-[2vw] lg:text-[1vw] font-semibold items-center justify-between">
                 <label htmlFor="name">Name</label>
                 <FaRegUser color="#FF6004" />
               </div>
               <input
                 type="text"
-                className="md:text-[1.4vw] lg:text-[0.86vw] w-full montserrat"
+                className="text-[4vw] md:text-[1.4vw] lg:text-[0.86vw] w-full montserrat"
                 placeholder="abc surname"
               />
             </div>
             <div className="bg-white grow py-2 rounded-md shadow-md px-4">
-              <div className="flex montserrat md:text-[2vw] lg:text-[1vw] font-semibold items-center justify-between">
+              <div className="flex montserrat text-[4.5vw] md:text-[2vw] lg:text-[1vw] font-semibold items-center justify-between">
                 <label htmlFor="email">Email</label>
                 <FaRegUser color="#FF6004" />
               </div>
               <input
                 type="text"
-                className="md:text-[1.4vw] lg:text-[0.86vw] w-full montserrat"
+                className="text-[4vw] md:text-[1.4vw] lg:text-[0.86vw] w-full montserrat"
                 placeholder="abc@gmail.com"
               />
             </div>
           </div>
-          <div className="flex gap-4 my-8 flex-wrap">
+          <div className="flex gap-4 my-4 md:my-8 flex-wrap">
             <div className="bg-white py-2 grow rounded-md shadow-md px-4">
-              <div className="flex montserrat md:text-[2vw] lg:text-[1vw] font-semibold items-center justify-between">
+              <div className="flex montserrat text-[4.5vw] md:text-[2vw] lg:text-[1vw] font-semibold items-center justify-between">
                 <label htmlFor="dob">Date of Birth</label>
                 <FaRegUser color="#FF6004" />
               </div>
               <input
                 type="text"
-                className="md:text-[1.4vw] lg:text-[0.86vw] w-full montserrat"
+                className="text-[4vw] md:text-[1.4vw] lg:text-[0.86vw] w-full montserrat"
                 placeholder="07 July 2000"
               />
             </div>
             <div className="bg-white grow py-2 rounded-md shadow-md px-4">
-              <div className="flex montserrat md:text-[2vw] lg:text-[1vw] font-semibold items-center justify-between">
+              <div className="flex montserrat text-[4.5vw] md:text-[2vw] lg:text-[1vw] font-semibold items-center justify-between">
                 <label htmlFor="lang">Language</label>
                 <FaRegUser color="#FF6004" />
               </div>
               <input
                 type="text"
-                className="md:text-[1.4vw] lg:text-[0.86vw] w-full montserrat"
+                className="text-[4vw] md:text-[1.4vw] lg:text-[0.86vw] w-full montserrat"
                 placeholder="English(US)"
               />
             </div>
           </div>
-          <div className="flex gap-4 my-8 flex-wrap">
-            <div className="bg-white py-2 grow-[.37] rounded-md shadow-md px-4">
-              <div className="flex montserrat md:text-[2vw] lg:text-[1vw] font-semibold items-center justify-between">
+          <div className="flex gap-4 my-4 md:my-8 flex-wrap">
+            <div className="bg-white py-2 grow md:grow-[.37] rounded-md shadow-md px-4">
+              <div className="flex montserrat text-[4.5vw] md:text-[2vw] lg:text-[1vw] font-semibold items-center justify-between">
                 <label htmlFor="contact">Contact</label>
                 <FaRegUser color="#FF6004" />
               </div>
               <input
                 type="text"
-                className="md:text-[1.4vw] lg:text-[0.86vw] w-full  montserrat"
+                className="text-[4vw] md:text-[1.4vw] lg:text-[0.86vw] w-full  montserrat"
                 placeholder="+91 7766889900"
               />
             </div>
           </div>
-          <div className="flex gap-4 my-8 flex-wrap">
+          <div className="flex gap-4 my-4 md:my-8 flex-wrap">
             <div className="bg-white py-2 grow-[1] rounded-md shadow-md px-4">
-              <div className="flex montserrat md:text-[2vw] lg:text-[1vw] font-semibold items-center justify-between">
+              <div className="flex montserrat text-[4.5vw] md:text-[2vw] lg:text-[1vw] font-semibold items-center justify-between">
                 <label htmlFor="address01">Address 01</label>
                 <FaRegUser color="#FF6004" />
               </div>
               <input
                 type="text"
-                className="md:text-[1.4vw] lg:text-[0.86vw] w-full  montserrat"
+                className="text-[4vw] md:text-[1.4vw] lg:text-[0.86vw] w-full  montserrat"
                 placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in ero."
               />
             </div>
           </div>
-          <div className="flex gap-4 my-8 flex-wrap">
+          <div className="flex gap-4 my-4 md:my-8 flex-wrap">
             <div className="bg-white py-2 grow-[1] rounded-md shadow-md px-4">
-              <div className="flex montserrat md:text-[2vw] lg:text-[1vw] font-semibold items-center justify-between">
+              <div className="flex montserrat text-[4.5vw] md:text-[2vw] lg:text-[1vw] font-semibold items-center justify-between">
                 <label htmlFor="address02">Address02</label>
                 <FaRegUser color="#FF6004" />
               </div>
               <input
                 type="text"
-                className="md:text-[1.4vw] lg:text-[0.86vw] w-full montserrat"
+                className="text-[4vw] md:text-[1.4vw] lg:text-[0.86vw] w-full montserrat"
                 placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in ero."
               />
             </div>
           </div>
-          <div className="flex gap-4 my-8 flex-wrap">
+          <div className="flex gap-4 my-4 md:my-8 flex-wrap">
             <div className="bg-white py-2 grow rounded-md shadow-md px-4">
-              <div className="flex montserrat md:text-[2vw] lg:text-[1vw] font-semibold items-center justify-between">
+              <div className="flex montserrat text-[4.5vw] md:text-[2vw] lg:text-[1vw] font-semibold items-center justify-between">
                 <label htmlFor="country">Country/Region</label>
                 <FaRegUser color="#FF6004" />
               </div>
               <input
                 type="text"
-                className="md:text-[1.4vw] lg:text-[0.86vw] w-full montserrat"
+                className="text-[4vw] md:text-[1.4vw] lg:text-[0.86vw] w-full montserrat"
                 placeholder="india"
               />
             </div>
             <div className="bg-white grow py-2 rounded-md shadow-md px-4">
-              <div className="flex montserrat md:text-[2vw] lg:text-[1vw] font-semibold items-center justify-between">
+              <div className="flex montserrat text-[4.5vw] md:text-[2vw] lg:text-[1vw] font-semibold items-center justify-between">
                 <label htmlFor="state">State</label>
                 <FaRegUser color="#FF6004" />
               </div>
               <input
                 type="text"
-                className="md:text-[1.4vw] lg:text-[0.86vw] w-full montserrat"
+                className="text-[4vw] md:text-[1.4vw] lg:text-[0.86vw] w-full montserrat"
                 placeholder="Maharashtra"
               />
             </div>
           </div>
-          <div className="flex gap-4 my-8 flex-wrap">
-            <div className="bg-white py-2 grow-[.37] rounded-md shadow-md px-4">
-              <div className="flex montserrat md:text-[2vw] lg:text-[1vw] font-semibold items-center justify-between">
+          <div className="flex gap-4 my-4 md:my-8 flex-wrap">
+            <div className="bg-white py-2 grow md:grow-[.37] rounded-md shadow-md px-4">
+              <div className="flex montserrat text-[4.5vw] md:text-[2vw] lg:text-[1vw] font-semibold items-center justify-between">
                 <label htmlFor="pincode">Pincode</label>
                 <FaRegUser color="#FF6004" />
               </div>
               <input
                 type="text"
-                className="md:text-[1.4vw] lg:text-[0.86vw] w-full  montserrat"
+                className="text-[4vw] md:text-[1.4vw] lg:text-[0.86vw] w-full  montserrat"
                 placeholder="4000107"
               />
             </div>
           </div>
           <button
-            className="btn px-3 mt-8 mb-12 text-white shadow-md montserrat font-medium bg-[#781393] text-[3vw] md:text-[2vw] lg:text-[1vw]"
+            className="btn px-3 mt-8 mb-12 text-white shadow-md montserrat font-medium bg-[#781393] text-[3vw] text-[4.5vw] md:text-[2vw] lg:text-[1vw]"
             type="submit"
           >
             Save Changes
--- src/Components/Profile/ProfilePage/ProfilePage.jsx
+++ src/Components/Profile/ProfilePage/ProfilePage.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import SideBar from "../SideBar/SideBar";
 import PersonalInfo from "../PersonalInfo/PersonalInfo";
 
 const ProfilePage = () => {
+    const[activeTab,setActiveTab]=useState("edit-profile")
   return (
-    <div className="py-6  md:px-6 lg:px-12 bg-[#F7F7F7]">
+    <div className="py-6 px-4  md:px-6 lg:px-12 bg-[#F7F7F7]">
       <h1 className="Sans border-b  border-[#00000075] pb-2 text-black text-[6vw]  md:text-[4.5vw] font-bold md:leading-[4.8vw] lg:leading-[3.2vw] lg:text-[2.8vw]">
         Dialable Account
       </h1>
-      <div className="pt-6 flex items-start">
-        <SideBar/>
-        <PersonalInfo/>
+      <div className="pt-6 flex md:flex-row flex-col items-start">
+        <SideBar setActiveTab={setActiveTab} activeTab={activeTab}/>
+
+        {activeTab === "edit-profile" ? (
+          <PersonalInfo />
+        ) : activeTab === "order-history" ? (
+          <div className="h-[100vh] md:pl-6 md:border-l border-[#00000075]">Order History</div>
+        ) : activeTab === "logout" ? (
+          <div className="h-[100vh] md:pl-6 md:border-l border-[#00000075]">logout</div>
+        ) : null}
       </div>
     </div>
   );
