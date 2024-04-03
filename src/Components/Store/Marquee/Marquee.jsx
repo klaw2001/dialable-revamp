58dff3d703a5b412377a3d544de1b52980d1600b
munkeyyy <r2256308@gmail.com>
2024-04-03 15:57:20 +0530

    change animation

--- src/Components/Store/Marquee/Marquee.jsx
+++ src/Components/Store/Marquee/Marquee.jsx
@@ -2,10 +2,27 @@ import React from "react";
 
 const Marquee = () => {
   return (
-    <div className="bg-[#EDEBEB] my-14 md:my-24 py-6 w-full flex items-center gap-4">
-      <h1 className="Sans text-[4.5vw] md:text-[4.2vw] animate font-semibold uppercase text-[#777676]">
-        Free SHipping on all Handmade Products
-      </h1>
+    <div className="bg-[#EDEBEB] relative my-14 md:my-24 py-6 w-full flex items-center ">
+      <div className="flex justify-between overflow-visible whitespace-nowrap gap-10 flex-nowrap items-center">
+        <h1 className="Sans text-[4.5vw] md:text-[4.2vw]   animate  font-semibold uppercase w-[2000px] text-[#777676]">
+          Free SHipping on all Handmade Products
+        </h1>
+        <h1 className="Sans text-[4.5vw] md:text-[4.2vw]   animate  font-semibold uppercase w-[2000px] text-[#777676]">
+          Free SHipping on all Handmade Products
+        </h1>
+        <h1 className="Sans text-[4.5vw] md:text-[4.2vw] animate   font-semibold uppercase w-[2000px] text-[#777676]">
+          Free SHipping on all Handmade Products
+        </h1>
+        <h1 className="Sans text-[4.5vw] md:text-[4.2vw]  animate  font-semibold uppercase w-[2000px] text-[#777676]">
+          Free SHipping on all Handmade Products
+        </h1>
+        <h1 className="Sans text-[4.5vw] md:text-[4.2vw] animate   font-semibold uppercase w-[2000px] text-[#777676]">
+          Free SHipping on all Handmade Products
+        </h1>
+        <h1 className="Sans text-[4.5vw] md:text-[4.2vw]  animate  font-semibold uppercase w-[2000px] text-[#777676]">
+          Free SHipping on all Handmade Products
+        </h1>
+      </div>
     </div>
   );
 };
