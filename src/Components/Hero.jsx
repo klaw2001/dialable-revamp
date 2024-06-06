776eda7eb56a5044ed678a8924463c8f715a172e
klaw2001 <netke2611@gmail.com>
2024-06-06 16:45:11 +0530

    some minor fixes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,16 +7,7 @@ import text_speech from "../../public/Images/text-to-speech.svg";
 import Link from "next/link";
 const Hero = () => {
   return (
-    <div className="md:flex  justify-between lg:h-[100vh] xl:h-[72vh] items-center gap-10 montserrat hero-gradient block p-6  md:p-0 ">
-      <div className="hero-img md:w-[50.3%]  h-full w-full  mt-0 relative">
-        <Image
-          src={placeholder}
-          alt="hero-img"
-          loading="eager"
-          className="h-full object-cover w-full"
-          placeholder="blur"
-        />
-      </div>
+    <div className="md:flex max-w-screen-xl mx-auto justify-between lg:h-[100vh] xl:h-[72vh] items-center gap-10 montserrat block p-6  md:p-0 ">
       <div className="hero-cont  md:w-[45%] mt-8 md:mt-6 lg:mt-0  w-full flex items-start  gap-4 md:gap-5  flex-col  ">
         <button className="flex items-center gap-2 rounded-xl lg:gap-1 text-[2.3vw] md:text-[1.5vw] lg:text-[1vw] p-2 border-2 btn border-black text-black poppins uppercase font-semibold">
           <div className="w-[4vw] md:w-[2.5vw] lg:w-[1vw]">
@@ -31,15 +22,15 @@ const Hero = () => {
         <h1 className="md:text-[3vw] text-[8vw] leading-[11vw] md:leading-[3vw] Sans font-semibold text-[black]">
           Welcome to <span className="text-[#781393]">Dialable</span>
         </h1>
-        <div className="desc poppins w-[75%]">
+        <div className="desc Sans w-[75%]">
           <p className="text-[black] font-medium text-lg lg:text-[1vw] leading-[1.45rem] ">
-            <span className="text-[2.1vw] font-semibold">
+            <span className="text-[2.1vw] font-semibold Sans leading-[2rem]">
               Embrace diversities, celebrating inclusion!
             </span>
             <br />
           </p>
-          <p className="text-[1.5vw] mt-3 text-[black] font-semibold">
-            All inclusive hub and knowledge
+          <p className="text-[0.9vw] mt-3 text-[black] font-semibold poppins">
+          An all-inclusive hub of knowledge, fostering a community that values and respects the unique perspectives of every individual. Join us in creating a space where everyone feels welcomed, heard, and empowered.
           </p>
         </div>
         <div className="search flex gap-4 ">
@@ -61,7 +52,10 @@ const Hero = () => {
           </div>
         </div>
         <div className="mt-2 flex flex-col gap-3 items-start">
-          <Link href={"#"} className="text-black font-medium text-[.8vw] underline poppins">
+          <Link
+            href={"#"}
+            className="text-black font-medium text-[.8vw] underline poppins"
+          >
             click to watch our video
           </Link>
 
@@ -75,6 +69,16 @@ const Hero = () => {
           </button>
         </div>
       </div>
+      <div className="hero-img md:w-[50.3%]  h-full w-full  mt-0 relative">
+        <Image
+          src={placeholder}
+          alt="hero-img"
+          loading="eager"
+          className="h-full object-cover w-full"
+          placeholder="blur"
+        />
+      </div>
+      
     </div>
   );
 };
